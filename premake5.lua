39327661b616186773190c98a2675459e3fd9e2b
Malin Lindner <malindner99@t-online.de>
2025-04-29 17:54:23 +0200

    initial commit

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+solution "cocktail-party"
+  
+  editorintegration "On"
+  platforms { "x64", "ARM64" }
+
+  if (_ACTION == "gmake" or _ACTION == "gmake2") then
+    configurations { "Release", "Debug", "ReleaseClang", "DebugClang" }
+    linkgroups "On"
+    filter { "configurations:*Clang" }
+    toolset "clang"
+    filter { }
+  elseif os.target() == "macosx" then
+    configurations { "Release", "Debug" }
+    toolset "clang"
+  else
+    configurations { "Release", "Debug", "ReleaseClang", "DebugClang" }
+  end
+
+dofile "backend/project.lua"
