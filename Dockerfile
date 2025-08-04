0ed8795fd4d8f275e511a6b8e4768471770d273e
Malin Lindner <malindner99@t-online.de>
2025-08-04 15:13:38 +0200

    Updating dockerfile to install asio package, adding error exits to script

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,13 @@ RUN apt install -y llvm-18 clang-18 build-essential
 RUN update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-18 100
 RUN update-alternatives --install /usr/bin/llvm-profdata llvm-profdata /usr/bin/llvm-profdata-18 100 && \
 update-alternatives --install /usr/bin/llvm-cov llvm-cov /usr/bin/llvm-cov-18 100
+RUN apt install -y libasio-dev
 
 WORKDIR /app
 
-COPY backend ./backend
+COPY backend/src ./backend/src
+COPY backend/3rdParty ./backend/3rdParty
+COPY backend/project.lua ./backend/project.lua
 COPY buildscripts ./buildscripts
 COPY premake ./premake
 COPY premake5.lua .
--- buildscripts/coverage.sh
+++ buildscripts/coverage.sh
@@ -2,19 +2,27 @@
 
 echo "Generating Project Files..."
 premake/premake5 gmake2 --test-coverage
+if [ $? -ne 0 ]; then exit 1; fi
 
 echo "Building Release..."
 make clean
+if [ $? -ne 0 ]; then exit 1; fi
 config=releaseclang_x64 make
+if [ $? -ne 0 ]; then exit 1; fi
 
 echo "Building Debug..."
 make clean
+if [ $? -ne 0 ]; then exit 1; fi
 config=debugclang_x64 make
+if [ $? -ne 0 ]; then exit 1; fi
 
 echo "Running Tests, Generating Coverage..."
 LLVM_PROFILE_FILE="./backend/builds/bin/coverage.profraw" backend/builds/bin/cocktail-party --run-tests
+if [ $? -ne 0 ]; then exit 1; fi
 
 echo "Preparing..."
 llvm-profdata merge -sparse ./backend/builds/bin/coverage.profraw -o ./backend/builds/bin/coverage_merged.profdata
+if [ $? -ne 0 ]; then exit 1; fi
 llvm-cov report ./backend/builds/bin/cocktail-party -instr-profile=./backend/builds/bin/coverage_merged.profdata -show-region-summary=false -show-branch-summary=false backend/src/cocktail_party.cpp backend/src/cocktail_party.h
+if [ $? -ne 0 ]; then exit 1; fi
 #TOTAL.*?(\d+\.\d+%)$
