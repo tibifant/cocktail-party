461895e2041df6490e6562137c2fff21a92e18f1
Malin Lindner <malindner99@t-online.de>
2025-08-05 15:23:27 +0200

    not yet working dockerfile for frontend test

--- buildscripts/frontend-testing.sh
+++ buildscripts/frontend-testing.sh
@@ -4,6 +4,6 @@ echo "Running frontend Tests..."
 tmux new-session -d -s server-session 'backend/builds/bin/cocktail-party --run-frontend-tests'
 if [ $? -ne 0 ]; then tmux kill-session -t server-session; fi
 npx playwright test
-if [ $? -ne 0 ]; then exit 1; fi
+#if [ $? -ne 0 ]; then tmux kill-session -t server-session; exit 1; fi
 tmux kill-session -t server-session
 if [ $? -ne 0 ]; then exit 1; fi
