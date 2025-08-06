5bc4640ab109fd7500b1289a4c72056209af91ad
Malin Lindner <malindner99@t-online.de>
2025-08-06 12:40:06 +0200

    adapting gitlba ci

--- buildscripts/coverage.sh
+++ buildscripts/coverage.sh
@@ -19,4 +19,3 @@ llvm-profdata merge -sparse ./backend/builds/bin/coverage.profraw -o ./backend/b
 if [ $? -ne 0 ]; then exit 1; fi
 llvm-cov report ./backend/builds/bin/cocktail-party -instr-profile=./backend/builds/bin/coverage_merged.profdata -show-region-summary=false -show-branch-summary=false backend/src/cocktail_party.cpp backend/src/cocktail_party.h
 if [ $? -ne 0 ]; then exit 1; fi
-#TOTAL.*?(\d+\.\d+%)$
