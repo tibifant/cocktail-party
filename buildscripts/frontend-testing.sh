873eeb4c71a318ad2427a13d56a46bc2116c6252
Malin Lindner <malindner99@t-online.de>
2025-08-07 16:33:50 +0200

    debug help for tmux

--- buildscripts/frontend-testing.sh
+++ buildscripts/frontend-testing.sh
@@ -5,11 +5,16 @@ npm install -D @playwright/test@1.54.2
 npx playwright install-deps
 npx playwright install chromium
 
-echo "Running frontend Tests..."
+echo "Starting Server..."
+ls backend/builds/bin
 tmux new-session -d -s server-session 'backend/builds/bin/cocktail-party --run-frontend-tests'
-if [ $? -ne 0 ]; then tmux kill-session -t server-session; fi
+if [ $? -ne 0 ]; then tmux kill-session -t server-session; exit 1; fi
 tmux ls
+tmux capture-pane -t server-session -p
+echo "Running frontend Tests..."
 CI=1 npx playwright test
 if [ $? -ne 0 ]; then tmux kill-session -t server-session; exit 1; fi
+echo "Stopping Server..."
 tmux kill-session -t server-session
 if [ $? -ne 0 ]; then exit 1; fi
+tmux ls
