183830a76d9ed206d7254484d29c779fba490322
Malin Lindner <malindner99@t-online.de>
2025-08-09 01:49:03 +0200

    remove nginx

--- buildscripts/frontend-testing.sh
+++ buildscripts/frontend-testing.sh
@@ -6,13 +6,11 @@ npx playwright install-deps
 npx playwright install chromium
 
 echo "Starting Server..."
-nginx
-if [ $? -ne 0 ]; then nginx -s quit; exit 1; fi
+if [ $? -ne 0 ]; then exit 1; fi
 tmux new-session -d -s server-session 'backend/builds/bin/cocktail-party --run-frontend-tests'
-if [ $? -ne 0 ]; then nginx -s quit; tmux kill-session -t server-session; exit 1; fi
+if [ $? -ne 0 ]; then tmux kill-session -t server-session; exit 1; fi
 echo "Running Tests..."
 CI=1 npx playwright test
-if [ $? -ne 0 ]; then nginx -s quit; tmux kill-session -t server-session; exit 1; fi
+if [ $? -ne 0 ]; then tmux kill-session -t server-session; exit 1; fi
 #echo "Stopping Server..."
 tmux kill-session -t server-session
-nginx -s quit
